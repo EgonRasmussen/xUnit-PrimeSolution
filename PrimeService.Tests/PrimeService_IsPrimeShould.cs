5192e16a7c870319147b094ca016b277cd8bc6bf
ECR\EgonRasmussen <ecr@eucsyd.dk>
2022-01-10 17:53:15 +0100

    Add project files.

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,63 @@
+using Xunit;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Fact]
+        public void Isrime()
+        {
+            // Arrange
+            PrimeServiceLib.PrimeService _primeService = new PrimeServiceLib.PrimeService();
+            // Act
+            var result = _primeService.IsPrime(1);
+            // Assert
+            Assert.False(result, $"{1} should not be prime");
+        }
+
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            // Arrange
+            PrimeServiceLib.PrimeService _primeService = new PrimeServiceLib.PrimeService();
+            // Act
+            var result = _primeService.IsPrime(value);
+            // Assert
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
+        {
+            // Arrange
+            PrimeServiceLib.PrimeService _primeService = new PrimeServiceLib.PrimeService();
+            // Act
+            var result = _primeService.IsPrime(value);
+            // Assert
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
+        {
+            // Arrange
+            PrimeServiceLib.PrimeService _primeService = new PrimeServiceLib.PrimeService();
+            // Act
+            var result = _primeService.IsPrime(value);
+            // Assert
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# xUnit test
+
+ ### PrimeService
+
+Kode til demo af [Unit testing C# in .NET Core using dotnet test and xUnit](https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test)
