c9997f6026b0118adfb768668279295686ade452
EgonRasmussen <ecr@eucsyd.dk>
2022-11-15 08:42:07 +0100

    ok

--- PrimeServiceLib/PrimeService.cs
+++ PrimeServiceLib/PrimeService.cs
@@ -1,22 +1,21 @@
-﻿namespace PrimeServiceLib
+﻿namespace PrimeServiceLib;
+
+public class PrimeService
 {
-    public class PrimeService
+    public bool IsPrime(int candidate)
     {
-        public bool IsPrime(int candidate)
+        if (candidate < 2)
         {
-            if (candidate < 2)
+            return false;
+        }
+
+        for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+        {
+            if (candidate % divisor == 0)
             {
                 return false;
             }
-
-            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
-            {
-                if (candidate % divisor == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
+        return true;
     }
 }
